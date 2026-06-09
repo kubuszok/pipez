541ea9d00f19099f257e2b5e46fb58dc9bf5e83e
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2026-06-09 20:37:47 +0200

    WIP: Aux pattern with inferImplicitValue - Result type extraction needs identity case handling

--- pipez/src/main/scala-2/pipez/PipeSemiautoConfiguredSupport.scala
+++ pipez/src/main/scala-2/pipez/PipeSemiautoConfiguredSupport.scala
@@ -2,26 +2,16 @@ package pipez
 
 import scala.language.experimental.macros
 
-/** Mix-in providing `derive` method for semiautomatic `Pipe` derivation without recursion and custom configuration */
 trait PipeSemiautoConfiguredSupport[Pipe[_, _]] {
 
-  /** Derives `Pipe[In, Out]` using provided settings */
   def derive[In, Out](
       config: PipeDerivationConfig[Pipe, In, Out]
   )(implicit
       pipeDerivation: PipeDerivation[Pipe]
   ): Pipe[In, Out] = macro pipez.internal.compiletime.PipezMacro.deriveConfigured[Pipe, In, Out]
 
-  /** Utility useful for providing configuration to macro.
-    *
-    * Example: `TypeClass.derive(TypeClass.Config[In, Out].enableDiagnostics)`.
-    */
   object Config {
-
-    /** Initiates the config object. Should be used ONLY within `TypeClass.derive(...)` */
     def apply[In, Out]: Config[In, Out] = ???
-
-    /** Initiates the config object. Should be used ONLY within `TypeClass.derive(...)` */
     def empty[In, Out]: Config[In, Out] = ???
   }
   type Config[In, Out] = PipeDerivationConfig[Pipe, In, Out]
--- pipez/src/main/scala-2/pipez/PipeSemiautoSupport.scala
+++ pipez/src/main/scala-2/pipez/PipeSemiautoSupport.scala
@@ -2,10 +2,8 @@ package pipez
 
 import scala.language.experimental.macros
 
-/** Mix-in providing `derive` method for semiautomatic `Pipe` derivation without recursion and default config */
 trait PipeSemiautoSupport[Pipe[_, _]] {
 
-  /** Derives `Pipe[In, Out]` using default settings */
   def derive[In, Out](implicit
       pipeDerivation: PipeDerivation[Pipe]
   ): Pipe[In, Out] = macro pipez.internal.compiletime.PipezMacro.deriveDefault[Pipe, In, Out]
